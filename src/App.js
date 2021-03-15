1f58190dd962cfb901010eb0b5be8ed4cd7086e3
unknown <hasnain@optimusfox.com>
2021-03-15 18:40:05 +0500

    app.js file updated

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <h3>Digital Ocean Practice</h3>
+        <h3>Digital Ocean Practice Updated</h3>
       </header>
     </div>
   );
